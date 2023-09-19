65f99f9ce772733275563dfd4816b42404df1eab
DidasMba <daniel.mbarushi@gmail.com>
2023-09-19 11:19:14 +0300

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+
+// Import the code you want to test
+const currentUser = 'Dan';
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+const exciteWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser}!';
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
